db9480787223c0d42971d921eb4c80b782c49ad3
milovan <milovan.minic@arbor-education.com>
2016-01-12 16:17:53 +0100

    Added ResizeImage.php class
    Added only empty methods as for building the base of class.

---
+++ application/ResizeImage.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milovan.minic
+ * Date: 1/12/16
+ * Time: 16:08
+ */
+
+class ResizeImage {
+
+    /**
+     * Resize to exact width and height. Aspect ratio will not be maintained
+     */
+    public function resizeExact()
+    {
+
+    }
+
+    /**
+     * The best strategy (portrait or landscape) will be selected for a given image according to its aspect ratio
+     */
+    public function resizeAuto()
+    {
+
+    }
+
+    /**
+     * This option will crop your images to the exact size you specify with no distortion
+     */
+    public function resizeCrop()
+    {
+
+    }
+
+    /**
+     * This strategy will first crop the image by its shorter dimension to make it a square, then resize it to the specified
+     */
+    public function resizeSquare()
+    {
+
+    }
+
+}
