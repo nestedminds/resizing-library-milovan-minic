3bd82446b25b4af27369c64045fb881223840d94
milovan-minic <milovan.minic@gmail.com>
2016-06-13 13:46:44 +0200

    Refactor of Exact resize class

--- lib/Exact.php
+++ lib/Exact.php
@@ -15,22 +15,27 @@ class Exact
      */
     public function __construct($image, $width, $height)
     {
-        $saveLocation = __DIR__ . '/../images/exactResized';
-        $name = end(explode('/', $image));
-        $date = new \DateTime();
-        $dateTime = date_format($date, 'Y-m-d_H-i-s');
-        $fileName = 'exact_' . $width . 'x' . $height . '-' . $dateTime . '_' . $name;
-        $createFolder = true;
-        $backgroundColor = null;
-        $imageQuality = 95;
-
         // Initialize layer from existing image
         $layer = ImageWorkshop::initFromPath($image);
 
         // Resize picture to be squared
         $layer->resizeInPixel($width, $height);
 
+        // Get width and height of resized image
+        $width = $layer->getWidth();
+        $height = $layer->getHeight();
+
+        // Make save configuration parameters
+        $saveConfig = new SaveConfig($image, $width, $height, __CLASS__);
+        $config = $saveConfig->getConfiguration();
+
         // Save resized image
-        $layer->save($saveLocation, $fileName, $createFolder, $backgroundColor, $imageQuality);
+        $layer->save(
+            $config['saveLocation'],
+            $config['fileName'],
+            $config['createFolder'],
+            $config['backgroundColor'],
+            $config['imageQuality']
+        );
     }
 }
